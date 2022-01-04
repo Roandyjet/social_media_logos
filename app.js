9ce588fc23aea9c370e8b6806ddbc81239bbe28b
Rova <roandyjet@gmail.com>
2022-01-04 04:49:09 +0300

    Social_media_right_side v2

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const titreSpans = document.querySelectorAll("h1 span");
-const btns = document.querySelectorAll(".btn-first");
 const logo = document.querySelector(".logo");
 const medias = document.querySelectorAll(".bulle");
-const l1 = document.querySelector(".l1");
-const l2 = document.querySelector(".l2");
 
 window.addEventListener("load", () => {
   const TL = gsap.timeline({ paused: true });
